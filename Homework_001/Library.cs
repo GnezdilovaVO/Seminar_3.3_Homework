b4fb596900f7f8d8a324ff49433d50ceb5aa04a2
Valentina <valyapomortseva@gmail.com>
2023-03-03 15:39:35 +0700

    Перенесла методы в файл Library

---
+++ Homework_001/Library.cs
@@ -0,0 +1,38 @@
+public static class Library
+{
+    public static int GetNumber(string s)
+    {
+        Console.WriteLine("Введите число: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+    public static int GetBackNumber(int straightNumber)
+    {
+        int backNumber = 0;
+        while (straightNumber > 0)
+        {
+            backNumber = backNumber * 10 + straightNumber % 10;
+            straightNumber = straightNumber / 10;
+        }
+
+        return backNumber;
+    }
+    public static bool Sravni(int number1, int number2)
+    {
+        if (number1 == number2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static string Outcome(bool resultSravni)
+    {
+        if (resultSravni == true) { return "да"; }
+        else
+        {
+            return "нет";
+        }
+    }
+}
--- Homework_001/Program.cs
+++ Homework_001/Program.cs
@@ -1,37 +1,4 @@
-﻿int GetNumber(string s)
-{
-    Console.WriteLine("Введите число: ");
-    return Convert.ToInt32(Console.ReadLine());
-}
-int GetBackNumber (int straightNumber)
-{
-    int backNumber = 0;
-    while (straightNumber > 0)
-    {
-        backNumber = backNumber * 10 + straightNumber % 10;
-        straightNumber = straightNumber / 10;
-    }
-
-    return backNumber;
-}
-bool Sravni(int number1, int number2)
-{
-    if (number1 == number2)
-    {
-       return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-string Outcome(bool resultSravni)
-{
-    if (resultSravni == true) {return "да";}
-    else {
-        return "нет";
-        }
-}
+﻿using static Library;
 int number = GetNumber("Введите число: ");
 int reserve = number;
 int newNumber = GetBackNumber(number);
