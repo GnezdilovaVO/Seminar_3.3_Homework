a49d920b496faddaba5a49597eac790e3b2a28ac
Valentina <valyapomortseva@gmail.com>
2023-03-03 14:44:41 +0700

    Решена задача 1 и исправлена блок-схема

--- Homework_001/Program.cs
+++ Homework_001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int newNumber = 0;
+int reserve = number;
+while (number > 0)
+{   
+    newNumber = newNumber*10 + number%10;
+    number = number/10;
+}
+if (reserve == newNumber) 
+{
+     Console.WriteLine("да");
+}
+else
+{
+   Console.WriteLine("нет");
+}
